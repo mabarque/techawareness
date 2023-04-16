7b1858705db9b89c68bda1fd2c6e5b651c7205a5
mabarque <maria_leiton@hotmail.com>
2023-04-15 21:34:56 -0600

    commit message

--- my-app-main2/babel.config.js
+++ my-app-main2/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', '@babel/preset-react']
+    presets: ['babel-preset-expo'],
+    plugins: ['@babel/plugin-syntax-jsx']
   };
 };
